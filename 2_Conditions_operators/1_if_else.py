ad02e514a54cb0746ab7a0f4b4e063d9ec9a6f8b
karansolase <126546501+karansolase@users.noreply.github.com>
2026-09-22 22:27:10 +0530

    if_else_match

---
+++ 2_Conditions_operators/1_if_else.py
@@ -0,0 +1,22 @@
+if 6 % 2 != 0:
+    print("Even")
+else :
+    print("Odd")
+
+salary = 2
+
+if salary > 5:
+    print("Great Job")
+elif salary == 5:
+    print("Medium Job")
+elif salary < 5:
+    print("Poor Job")
+
+
+score = input("ENter u r Score : ")
+
+x = 5
+print(x > 2 and x < 10)
+print(x > 10 or x < 10)
+
+print(not(x > 1))
---
+++ 2_Conditions_operators/2_match.py
@@ -0,0 +1,10 @@
+
+num = 3
+
+match num :
+    case 1 :
+        print("One")
+    case 2 : 
+        print("Two")
+    case 3 : 
+        print("Three")
